73de7645364d10da9c51683511481a5f91aaf8a9
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-04 15:27:17 -0700

    Add edit review to shelter. Test pass.
    Co-authored-by: sasloan <sebastiansloan94@gmail.com>
    Co-authored-by: DeMarcus Kirby <36940278+KirbyDD@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,6 +12,18 @@ class ReviewsController < ApplicationController
     redirect_to "/shelters/#{shelter.id}"
   end
 
+  def edit
+    @shelter = Shelter.find(params[:shelter_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update 
+    review = Review.find(params[:id])
+    review.update(review_params)
+    review.save
+    redirect_to "/shelters/#{review.shelter_id}"
+  end
+
   private
   def review_params
     params.permit(:title, :rating, :content)
