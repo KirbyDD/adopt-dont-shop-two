74252358fd24e320abf16c0f8b001861b3f87283
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-03 18:09:02 -0700

    Add has many relationship to shelter
    Co-authored-by: sasloan <sebastiansloan94@gmail.com>

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,5 @@ class Shelter < ApplicationRecord
    validates_presence_of :name, :address, :state, :city, :zip
 
    has_many :pets, dependent: :delete_all
+   has_many :reviews, dependent: :delete_all
 end
