2df3360ddde4af153d5349cc110f3b28ae56859c
sasloan <sebastiansloan94@gmail.com>
2019-12-05 14:49:28 -0700

    favorites controller edit is functioning
    Co-Authored-By: DeMarcus Kirby <KirbyDD@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,16 @@ class FavoritesController < ApplicationController
 
 	def update
 		pet = Pet.find(params[:pet_id])
-		flash[:notice] = "You have added #{pet.name} to your favorites."
-		redirect_to "/pets"
+		pet_id_str = pet.id.to_s
+		session[:favorites] ||= Hash.new(0)
+		session[:favorites][pet_id_str] ||= 0
+		session[:favorites][pet_id_str] += 1
+		if session[:favorites][pet_id_str] == 1
+		  flash[:notice] = "You have added #{pet.name} to your favorites."
+		  redirect_to "/pets"
+		else
+			flash[:notice] = "You have already added #{pet.name} in your favorites."
+			redirect_to "/pets"
+		end
 	end
 end
