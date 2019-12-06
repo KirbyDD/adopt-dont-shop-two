1bf3da67873a96e867eb76e332c10bf6555ac7e2
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-05 17:15:37 -0700

    Fix optional picture

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,7 +5,6 @@ describe Review, type: :model do
         it {should validate_presence_of :title}
         it {should validate_presence_of :rating}
         it {should validate_presence_of :content}
-        it {should validate_presence_of :optional_picture}
 
     end
     describe "relationships" do
