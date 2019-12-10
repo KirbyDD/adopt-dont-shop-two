24e3a4edfc5b1b9f378ece683ca702023681acdf
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-10 11:47:43 -0700

    Refactor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,18 @@ freja = ddfl.pets.create(image: "https://vetstreet.brightspotcdn.com/dims4/defau
 maggie = arvada_shelters.pets.create(image: "https://render.fineartamerica.com/images/rendered/default/poster/8/10/break/images/artworkimages/medium/1/awaiting-spring-black-lab-puppy-black-dog-art-judy-burrows.jpg", name: "Maggie", approximate_age: 12, sex: "female", description: "Black Lab")
 #
 #Applications
-sebastian = twitch.applications.create!(name: "Sebastian Sloan", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
-demarcus = jona.applications.create!(name: "DeMarcus Kirby", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
-ray = ozzy.applications.create!(name: "Ray Nguyen", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
 
+sebastian = Applications.create(name: "Sebastian Sloan", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
+demarcus = Applications.create(name: "DeMarcus Kirby", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
+ray = Applications.create(name: "Ray Nguyen", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
+fredo = Application.create(name: "Alfredo Ferradas", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
 
-
-
-fredo = Application.create(name: "Fredo Ferradas", address: "123 Fake st.", city: "Denver", state: "Colorado", zip: 80230, phone_number: "1234567890", why_would_you_make_a_good_pet_owner: "Because why not")
 
 PetApplication.create(pet: twitch, application: fredo)
+PetApplication.create(pet: jona, application: fredo)
+PetApplication.create(pet: ozzy, application: ray)
+PetApplication.create(pet: maggie, application: ray)
+PetApplication.create(pet: jona, application: ray)
+PetApplication.create(pet: jona, application: demarcus)
+PetApplication.create(pet: twitch, application: sebastian)
+
